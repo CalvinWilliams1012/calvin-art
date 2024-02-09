5ca502fcb20a04de0de025b72b9494ed5ea5e5d0
Calvin Williams <calvin.william1012@gmail.com>
2024-02-08 23:45:11 -0500

    adding coin section to dnd page

---
+++ src/pages/api/bankdata.ts
@@ -0,0 +1,11 @@
+import { supabase } from '../../lib/supabase.ts'
+export const prerender = false;
+
+export async function GET({ params }) {
+  const { data } = await supabase.from('Bank').select("*");
+  return new Response(
+    JSON.stringify({
+      coins: data,
+    }),
+  )
+}
