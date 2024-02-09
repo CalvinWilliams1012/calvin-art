e0c36fab8fca6e18d6e32e1a7abd3b7d76b7f1d9
Calvin Williams <calvin.william1012@gmail.com>
2024-02-09 01:58:59 -0500

    forgot prerender false on sign in..

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
 import type { Provider } from "@supabase/supabase-js";
+export const prerender = false;
 
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   const formData = await request.formData();
