7aba86134f9137c8ae995184623920ec315b6b5f
Calvin Williams <calvin.william1012@gmail.com>
2024-02-10 01:57:09 -0500

    added admin page to add role to user, and method to check user to use on protected pages

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@ interface ImportMetaEnv {
     readonly SUPABASE_URL: string
     readonly SUPABASE_ANON_KEY: string
     readonly API_HOST: string
+    readonly SUPABASE_SERVICE_ROLE: string
   }
   
   interface ImportMeta {
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+
 export const prerender = false;
+
 export const supabase = createClient(
   import.meta.env.SUPABASE_URL,
   import.meta.env.SUPABASE_ANON_KEY,
@@ -9,3 +11,47 @@ export const supabase = createClient(
     },
   },
 );
+
+const supabaseServiceRole = createClient(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_SERVICE_ROLE
+);
+
+
+export async function getUser(cookies){
+  
+  const accessToken = cookies.get("sb-access-token");
+  const refreshToken = cookies.get("sb-refresh-token");
+
+  //If we dont have the session cookies, return null
+  if (!accessToken || !refreshToken) {
+    return null;
+  }
+
+  //Hit supabase, throws error if invalid session, or refreshes on expired
+  const { data, error } = await supabase.auth.setSession({
+    refresh_token: refreshToken.value,
+    access_token: accessToken.value,
+  });
+
+  //invalid session, delete invalid cookies and return null
+  if (error) {
+    cookies.delete("sb-access-token", {
+      path: "/",
+    });
+    cookies.delete("sb-refresh-token", {
+      path: "/",
+    });
+
+    return null;
+  }
+  // return the session user.
+  return data?.user;
+}
+
+export async function addBonobo(userId){
+  return await supabaseServiceRole.auth.admin.updateUserById(
+    userId,
+    { app_metadata: { role: 'bonobo' } }
+  );
+}
