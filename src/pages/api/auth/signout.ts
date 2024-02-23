1741435d96b33e90babdc9f29cca3946033a0353
Calvin Williams <calvin.william1012@gmail.com>
2024-02-23 00:24:02 -0500

    ensure to call sign out api on sign out as well as delete cookies.

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,7 +1,9 @@
 import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase";
 export const prerender = false;
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
+  const {error} = await supabase.auth.signOut();
   cookies.delete("sb-access-token", { path: "/" });
   cookies.delete("sb-refresh-token", { path: "/" });
   return redirect("/auth/signin");
